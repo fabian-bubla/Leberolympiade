c3da457e8937498235e0bedac390dd3fca23b2ff
fabian-bubla <fabian.bubla@gmail.com>
2021-05-21 17:17:54 +0200

    random_hands

---
+++ assets/Hands/HandSprite.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+
+func _ready():
+#	pass
+	while true:
+		randomize()
+		var rnd_idx = randi() % 17
+		if !Stats.Sprite_idx_already_in_use.has(rnd_idx):
+			self.frame = rnd_idx
+			Stats.Sprite_idx_already_in_use.append(rnd_idx)
+			break
+#		self.frame = Stats.Sprite_idx_list[random_sprite_idx]
+#		Stats.Sprite_idx_list.remove(random_sprite_idx)
+#		pass # Replace with function body.
+
--- assets/Stats/Stats.gd
+++ assets/Stats/Stats.gd
@@ -2,3 +2,8 @@ extends Node
 
 
 var max_score = 20
+var Sprite_idx_list = range(0,17)
+var Sprite_idx_already_in_use = []
+func _ready():
+	print(Sprite_idx_list)
+	pass
